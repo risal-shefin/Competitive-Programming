20d2daafbd12d770c0829be6ab1d8dd8b9f47895
Risal Shahriar Shefin <35737627+Shefin-CSE16@users.noreply.github.com>
2021-04-14 16:45:42 +0600

    Update SOS.cpp

--- Templates/SOS.cpp
+++ Templates/SOS.cpp
@@ -25,7 +25,6 @@ for(int mask = 0; mask < (1<<N); mask++){
 	F[mask] = dp[mask][N];
 }
 
-
 /// memory optimized, super easy to code.
 for(int i = 0; i<(1<<N); i++)
 	F[i] = A[i];
@@ -35,4 +34,3 @@ for(int i = 0;i < N; ++i) {
             F[mask] += F[mask^(1<<i)];
     }
 }
-
