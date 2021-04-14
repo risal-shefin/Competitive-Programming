005468a325a20f80c41162fdf45a3005d769fabe
Risal Shahriar Shefin <35737627+Shefin-CSE16@users.noreply.github.com>
2021-04-14 16:49:37 +0600

    Update SOS.cpp

--- Templates/SOS.cpp
+++ Templates/SOS.cpp
@@ -5,29 +5,29 @@
 /// Suboptimal Bruteforce Method O(3^n):
 // iterate over all the masks
 for (int mask = 0; mask < (1<<n); mask++) {
-	F[mask] = A[0];
+    F[mask] = A[0];
     // iterate over all the subsets of the mask
     for(int i = mask; i > 0; i = (i-1) & mask){
-    	F[mask] += A[i];
+        F[mask] += A[i];
     }
 }
 
 /// Two DP methods O(n*2^n):
 /// iterative version
 for(int mask = 0; mask < (1<<N); mask++){
-	dp[mask][0] = A[mask];	//handle base case separately (leaf states)
-	for(int i = 0;i < N; i++){
-		if(mask & (1<<i))
-			dp[mask][i + 1] = dp[mask][i] + dp[mask^(1<<i)][i];
-		else
-			dp[mask][i + 1] = dp[mask][i];
-	}
-	F[mask] = dp[mask][N];
+    dp[mask][0] = A[mask];	//handle base case separately (leaf states)
+    for(int i = 0;i < N; i++){
+        if(mask & (1<<i))
+            dp[mask][i + 1] = dp[mask][i] + dp[mask^(1<<i)][i];
+        else
+            dp[mask][i + 1] = dp[mask][i];
+    }
+    F[mask] = dp[mask][N];
 }
 
 /// memory optimized, super easy to code.
 for(int i = 0; i<(1<<N); i++)
-	F[i] = A[i];
+    F[i] = A[i];
 for(int i = 0;i < N; ++i) {
     for(int mask = 0; mask < (1<<N); ++mask){
         if(mask & (1<<i))
