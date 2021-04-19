690856d5aa2cab70398fb593d7209de63c946c45
Risal Shahriar Shefin <35737627+Shefin-CSE16@users.noreply.github.com>
2021-04-19 17:55:49 +0600

    Create Kuhn's Algorithm.cpp

---
+++ Templates/Kuhn's Algorithm.cpp
@@ -0,0 +1,29 @@
+// For Maximum Bipartitie Matching
+// Complexity: O(min(n*m, n^3))
+
+ll n, k; // n = left part size, k = right part size;
+vector <ll> g[1500], lft, rgt;
+vector <bool> used;
+
+bool try_kuhn(ll v)
+{
+    for (ll &to : g[v]) {
+        if(used[to]) continue;
+        used[to] = 1;
+
+        if(rgt[to]==-1 || try_kuhn(rgt[to])) {
+            lft[v] = to, rgt[to] = v;
+            return true;
+        }
+    }
+    return false;
+}
+
+void kuhn()
+{
+    lft.assign(n+1, -1), rgt.assign(k+1, -1);
+    for(ll v=1; v<=n; ++v) {
+        used.assign(k+1, false);
+        try_kuhn(v);
+    }
+}
