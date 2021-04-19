17cdc5141a9574f3e5078e883549b956645efacc
Risal Shahriar Shefin <35737627+Shefin-CSE16@users.noreply.github.com>
2021-04-19 18:12:24 +0600

    Update Minimum Enclosing Circle.cpp

--- Templates/Minimum Enclosing Circle.cpp
+++ Templates/Minimum Enclosing Circle.cpp
@@ -15,33 +15,30 @@ ll n;
 double X, Y, d, e;
 
 double dist(double a, double b) {
-	return a*a + b*b;
+    return a*a + b*b;
 }
 
-int main() {
+int main()
+{
+    scanf("%lld", &n);
+    for (ll i = 0; i < n; i++) {
+        scanf("%lld %lld", &p[i].x, &p[i].y);
+        X += p[i].x; Y += p[i].y;
+    }
+    X /= n; Y /= n;   /// Average center
 
-	scanf("%lld", &n);
-	for (ll i = 0; i < n; i++) {
-		scanf("%lld %lld", &p[i].x, &p[i].y);
-		X += p[i].x; Y += p[i].y;
-	}
-	X /= n; Y /= n;   /// Average center
-
-	double mv = 0.1;
-	for (ll i = 0; i < iter; i++) {
-		ll f = 0;
-		d = dist(X - p[0].x, Y - p[0].y);
-		for (ll j = 1; j < n; j++) {
-
-			e = dist(X - p[j].x, Y - p[j].y);
-			if (d < e) { d = e; f = j; }
-		}
-
-		/// Moving center towards the farthest point slightly
-		X += (p[f].x - X) * mv;
-		Y += (p[f].y - Y) * mv;
-		mv *= 0.999;
-	}
-
-	printf("X = %.3f Y = %.3f , radius = %.3f\n", X, Y, sqrt(d));
+    double mv = 0.1;
+    for (ll i = 0; i < iter; i++) {
+        ll f = 0;
+        d = dist(X - p[0].x, Y - p[0].y);
+        for (ll j = 1; j < n; j++) {
+            e = dist(X - p[j].x, Y - p[j].y);
+            if (d < e) { d = e; f = j; }
+        }
+        /// Moving center towards the farthest point slightly
+        X += (p[f].x - X) * mv;
+        Y += (p[f].y - Y) * mv;
+        mv *= 0.999;
+    }
+    printf("X = %.3f Y = %.3f , radius = %.3f\n", X, Y, sqrt(d));
 }
