434f03b2903164f4ae6be724550f2cbef17c677b
Risal Shahriar Shefin <35737627+Shefin-CSE16@users.noreply.github.com>
2022-05-08 18:19:51 +0600

    Update Maximum Bipartite Matching - Kuhn's Algorithm.cpp

--- Templates/Maximum Bipartite Matching - Kuhn's Algorithm.cpp
+++ Templates/Maximum Bipartite Matching - Kuhn's Algorithm.cpp
@@ -29,3 +29,51 @@ ll kuhn()
     }
     return max_match;
 }
+
+/* Optimized Kuhn's Algorithm. Blog: https://codeforces.com/blog/entry/17023 */
+ll kuhn2()
+{
+    ll max_match = 0;
+    lft.assign(l_siz+1, -1), rgt.assign(r_siz+1, -1);
+    
+    vector <ll> lft_part;
+    for(ll v=1; v<=l_siz; ++v) lft_part.push_back(v);
+    
+    // Shuffle the left part randomly to traverse them randomly
+    mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count()); // Random Seed
+    shuffle(lft_part.begin(), lft_part.end(), rng);
+    
+    // Greedy matching with adjacent nodes at first
+    for(auto &v : lft_part) {
+        // Shuffle the adjacent nodes to match them randomly
+        shuffle(g[v].begin(), g[v].end(), rng);
+        
+        for(auto &to : g[v]) {
+            if(rgt[to] == -1) {
+                lft[v] = to, rgt[to] = v;
+                max_match++;
+                break;
+            }
+        }
+    }
+    
+    // Main Kuhn's Algorithm Part
+    bool new_mat = 1;
+    while(new_mat) {
+        // used is cleared one time in each iteration so that
+        // we can find several paths in O(E).
+        used.assign(r_siz+1, false);
+        // If no new match is found, the loop will break
+        new_mat = 0;
+
+        for(auto &v : lft_part) {
+            if(lft[v] != -1)
+                continue;
+            
+            bool got = try_kuhn(v);
+            max_match += got, new_mat |= got;
+        }
+    }
+    
+    return max_match;
+}
